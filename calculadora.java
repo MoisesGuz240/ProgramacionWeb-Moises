bb715c62444fe1268e47ac51884e7a7dd6fd39e8
Moises Guzman Sotelo <moisesguz924@gmail.com>
2024-09-01 19:32:16 -0600

    esta tarea 5

---
+++ calculadora.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class calculadora{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("ingresa el primer numero: ");
+        double num1= scanner.nextDouble();
+        System.out.println("ingresa el segundo numero: ");
+        double num2= scanner.nextDouble();
+        double resultado=0;
+        System.out.print("Selecciona la operación (+, -, *, /): ");
+        char operacion = scanner.next().charAt(0);
+
+        switch (operacion) {
+            case '+':
+                resultado = num1 + num2;
+                break;
+            case '-':
+                break;
+            case '*':
+              
+                break;
+            case '/':
+                break;
+        }
+
+        System.out.println("El resultado de " + num1 + " " + operacion + " " + num2 + " es: " + resultado);
+    }
+}
